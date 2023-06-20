1e81edcd2862b52537fbd516aebadf01183bae56
nitiphron <122677663+nitiphron@users.noreply.github.com>
2023-06-20 21:29:20 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com", "drive.google.com","i.dummyjson.com"],
+  },
+};
+
+module.exports = nextConfig;
+
+
